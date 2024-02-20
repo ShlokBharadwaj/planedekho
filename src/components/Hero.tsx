646805a3580e800da3f3c7debd847607bc8a5ccb
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-02-20 13:24:17 +0530

    Add: Custom button component and Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Image from "next/image";
+import { CustomButton } from ".";
 
 const Hero = () => {
     return (
-        <div className="hero">Hero</div>
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">Find your right airplane — Swiftly and Seamlessly!</h1>
+
+                <p className="hero__subtitle">Find the perfect wings to take you to new heights.</p>
+
+                <CustomButton />
+            </div>
+        </div>
     )
 }
 
