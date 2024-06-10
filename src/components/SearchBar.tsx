934c00b28e6f7f61ff2ded4506e61c898da7b38c
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-06-10 12:23:47 +0530

    Update: URL with manufacturer search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,15 +3,37 @@
 import React, { useState } from 'react'
 import { ManufacturerSearch } from ".";
 import { SearchBarProps } from "@/types";
+import { useRouter } from "next/navigation";
 
 const SearchBar: React.FC<SearchBarProps> = ({ className }) => {
+
+  const router = useRouter();
 
   const [manufacturer, setManufacturer] = useState('')
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    console.log('searching...')
-  }
+    e.preventDefault();
+
+    if (manufacturer === '') {
+      return alert('Please enter a manufacturer');
+    }
+
+    updateSearchParams(manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (manufacturer: string) => {
+    const params = new URLSearchParams(window.location.search);
+
+    if (manufacturer) {
+      params.set('manufacturer', manufacturer);
+    } else {
+      params.delete('manufacturer');
+    }
+
+    const newPath = `${window.location.pathname}?${params.toString()}`;
+
+    router.push(newPath);
+  };
 
   return (
     <form className={`flex items-center justify-start max-sm:flex-col w-full relative max-sm:gap-4 max-w-3xl ${className}`} onSubmit={handleSearch} >
