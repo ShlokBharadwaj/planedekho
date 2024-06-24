1e0a02fcc1e0bc74c0f17f20e416b0bd73d4529e
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2024-06-24 17:45:36 +0530

    Add: Display custom filter items from array

--- src/components/CustomFilter.tsx
+++ src/components/CustomFilter.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, Fragment } from "react";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 
@@ -9,8 +10,25 @@ import { CustomFilterProps } from "@/types"
 
 
 const CustomFilter: React.FC<CustomFilterProps> = ({ title, options }) => {
+
+  const [selectedItem, setselectedItem] = useState(options[0]);
+
   return (
-    <div>{title}</div>
+    <div className="w-fit">
+      <Listbox
+        value={selectedItem}
+        onChange={(value) => setselectedItem(value)}
+      >
+        <div className="relative w-fit z-50">
+          <Listbox.Button className="relative w-full min-w-[127px] flex justify-between items-center cursor-default rounded-lg bg-gray-800 py-2 px-3 text-left shadow-md sm:text-sm border">
+            <span className="block truncate">{title}</span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+              <FontAwesomeIcon icon="chevron-down" className="w-5 h-5 text-gray-400" />
+            </span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
